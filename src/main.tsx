1f0748e5a499abdfa468d34f678f312c4649df50
yujiseok <koesijuy@gmail.com>
2023-03-02 20:39:22 +0900

    feat: 리액트 쿼리, zod 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { store } from "app/store";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </Provider>,
 );
